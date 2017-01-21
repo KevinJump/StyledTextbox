f48c643b0528834193cbd5e460cd15b3ac2a8f22
Kevin Jump <kevin@thejumps.co.uk>
2017-01-21 10:13:00 +0000

    Initial Commit for v1.3

---
+++ Distrib/styledtextbox/styledTextController.js
@@ -0,0 +1,49 @@
+﻿function simpleStyledTextController($scope, localizationService) 
+{
+    $scope.limit = parseInt($scope.model.config.charCount);
+
+    $scope.model.hideLabel = ($scope.model.config.hideLabel == 1);
+
+    $scope.getClass = function () {
+        if ($scope.limit > 0 && $scope.model.value != undefined)
+        {
+            if ($scope.model.value.length >= $scope.limit)
+            {
+                return 'text-danger';
+            }
+            return 'muted';
+        }
+    }
+
+    $scope.limitMsg = localizationService.dictionary['styledtext_limit'] || 
+        'you have reached the limit of {{limit}} charecters';
+    $scope.adviseMsg = localizationService.dictionary['styledtext_advice'] ||
+        'you have gone over the advised length by {{over}} charecters';
+
+    $scope.remaining = localizationService.dictionary['styledtext_remaining'] ||
+        '{{remain}} charecters left';
+
+    $scope.checkCharLimit = function () {
+        if ($scope.limit > 0 && $scope.model.value != undefined) {
+            if ($scope.model.value.length > $scope.limit) {
+
+                if ($scope.model.config.enforceLimit == 1) {
+                    $scope.model.value = $scope.model.value.substr(0, $scope.limit);
+                    $scope.info = $scope.limitMsg.replace('{{limit}}', $scope.limit);
+                }
+                else {
+                    $scope.info = $scope.adviseMsg.replace('{{over}}', ($scope.model.value.length -$scope.limit));
+                }
+            }
+            else {
+                $scope.info = $scope.remaining.replace('{{remain}}', ($scope.limit - $scope.model.value.length));
+            }
+
+        }
+    }
+
+    $scope.checkCharLimit();
+}
+
+angular.module('umbraco')
+    .controller('jumoo.propEditor.styledTextController', simpleStyledTextController);
---
+++ Jumoo.StyledTextbox/StyledTextBoxPropertyConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Jumoo.StyledTextbox
+{
+    [PropertyValueType(typeof(string))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+    public class StyledTextBoxPropertyConverter :
+        PropertyValueConverterBase         
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("Styled.TextBox");
+        }
+
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source;
+        }
+
+        public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source ?? string.Empty;
+        }
+
+        public override object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source; 
+        }
+    }
+}
